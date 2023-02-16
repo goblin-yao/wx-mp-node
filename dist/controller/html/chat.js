9c28b3256f866715ac2f9ada589ac12dfe7656dc
Junping Yao <junping.yao@seamoney.com>
2023-02-16 14:38:11 +0800

    update cl

--- dist/controller/html/chat.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    async chat(ctx) {
-        await ctx.render("chat");
-    },
-};
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiY2hhdC5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbIi4uLy4uLy4uL2NvbnRyb2xsZXIvaHRtbC9jaGF0LnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7O0FBRUEsa0JBQWU7SUFDYixLQUFLLENBQUMsSUFBSSxDQUFDLEdBQVk7UUFDckIsTUFBTSxHQUFHLENBQUMsTUFBTSxDQUFDLE1BQU0sQ0FBQyxDQUFDO0lBQzNCLENBQUM7Q0FDRixDQUFDIiwic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0IHsgQ29udGV4dCB9IGZyb20gXCJrb2FcIjtcblxuZXhwb3J0IGRlZmF1bHQge1xuICBhc3luYyBjaGF0KGN0eDogQ29udGV4dCk6IFByb21pc2U8dm9pZD4ge1xuICAgIGF3YWl0IGN0eC5yZW5kZXIoXCJjaGF0XCIpO1xuICB9LFxufTtcbiJdfQ==
